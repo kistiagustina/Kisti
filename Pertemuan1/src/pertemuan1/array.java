d63175d8f4e69baa64507d6853ce8f81f3c7127e
kistiagustina <145627738+kistiagustina@users.noreply.github.com>
2023-11-03 10:20:40 +0700

    Add files via upload

---
+++ Pertemuan1/src/pertemuan1/array.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan1;
+import java.util.Scanner;
+/**
+ *
+ * @author KISTIII
+ */
+public class array {
+
+public class Array {
+public static void main(String[] args) {
+        String[] Hari = {"Senin","Selasa","Rabu","Kamis","Jum'at"};
+
+   System.out.println("Mengakses Data Array:");
+   System.out.println("Hari ke 1: " + Hari[0]);
+   System.out.println("Hari ke 2: " + Hari[1]);
+   System.out.println("Hari ke 3: " + Hari[2]);
+   System.out.println("Hari ke 4: " + Hari[3]);
+   System.out.println("Hari ke 5: " + Hari[4]);
+}
+}
+}
